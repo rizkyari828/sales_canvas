af2c7f23995210be2b2186868cc1dfef02948627
Rizky Ari Pratama <BSI50164@bsi.co.id>
2025-08-19 17:26:52 +0700

    update

--- lib/models/request/user_id_request.dart
+++ lib/models/request/user_id_request.dart
@@ -1,11 +1,17 @@
 import 'dart:convert';
 
 class UserIdRequest {
-  UserIdRequest({required this.id, this.page, this.limit});
+  UserIdRequest({
+    required this.id,
+    this.page,
+    this.limit,
+    this.type, // tambahkan field opsional
+  });
 
   String id;
   String? page;
   String? limit;
+  String? type; // field opsional
 
   factory UserIdRequest.fromRawJson(String str) =>
       UserIdRequest.fromJson(json.decode(str));
@@ -13,8 +19,24 @@ class UserIdRequest {
   String toRawJson() => json.encode(toJson());
 
   factory UserIdRequest.fromJson(Map<String, dynamic> json) => UserIdRequest(
-      id: json["user_id"], page: json["page"], limit: json["limit"]);
+        id: json["user_id"],
+        page: json["page"],
+        limit: json["limit"],
+        type: json["type"], // mapping dari json jika ada
+      );
 
-  Map<String, dynamic> toJson() =>
-      {"user_id": id, "page": page, "limit": limit};
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{
+      "user_id": id,
+      "page": page,
+      "limit": limit,
+    };
+
+    // hanya sertakan "type" kalau tidak null
+    if (type != null) {
+      data["type"] = type;
+    }
+
+    return data;
+  }
 }
